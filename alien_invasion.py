129ba44e3190ec01992fd75a8086c3713eed9f23
=Winston bravo <wbravoanoni@gmail.com>
2024-05-02 00:45:00 -0400

    iniciar nave, mov izquierda y derecha

---
+++ alien_invasion.py
@@ -0,0 +1,54 @@
+from settings import Settings
+from ship import Ship
+
+import sys
+import pygame
+
+class AlienInvation:
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.bg_color = self.settings.bg_color
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+
+
+    def run_game(self):
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+           
+        
+        
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+                   
+
+    def _update_screen(self):
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
+
+
+
+
+
+if __name__ == '__main__':
+    ai = AlienInvation()
+    ai.run_game()
