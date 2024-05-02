c962c24f1c611365ffd08da410c2a851bd7dee3e
=Winston bravo <wbravoanoni@gmail.com>
2024-05-02 00:49:59 -0400

    Agregar paredes

--- ship.py
+++ ship.py
@@ -20,9 +20,9 @@ class Ship:
         self.moving_left = False
 
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x +=self.settings.ship_speed
-        elif self.moving_left:
+        elif self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         self.rect.x = self.x
